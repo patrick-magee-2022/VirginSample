4bef140e84ec0de87f28f9e47731d7cb5a627c9c
Patrick Magee <leafydogs@gmail.com>
2022-03-02 14:35:11 +0000

    filtered searches working.

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,12 +1,16 @@
 import { h } from 'preact';
 import { useState } from "preact/hooks";
-import style from './style.css';
-import { Container, Row, Col } from 'react-bootstrap';
-let sampleRequest = require("../profile/results.json");
+let sampleResults = require("../profile/results.json");
 // Note: `user` comes from the URL, courtesy of our router
 const SearchHolidays = () => {
     const [returnData, setReturnData] = useState(null);
     const [isFutureDate, setIsFutureDate] = useState(true);
+    const [isFiltered, setIsFiltered] = useState(false);
+    const [isFilteredByPrice, setIsFilteredByPrice] = useState(false);
+
+    const [isFilteredByStarRating, setIsFilteredByStarRating] = useState(false);
+    const [isFilteredByFacilities, setIsFilteredByFacilities] = useState(false);
+    let [sortedArray, setSortedArray] = useState(null);
     const cities = ["New York", "Orlando", "Barbados", "Toronto"];
     const currentDateTime = new Date();
     function checkDateField() {
@@ -24,7 +28,6 @@ const SearchHolidays = () => {
         let locationValue = event.target[0].value.toLowerCase();
         let dateValue = document.getElementById("inputDate").value;
         let checkDateValue = new Date(dateValue);
-        console.log("paddy", checkDateValue.toLocaleDateString('en-GB').replace(/\//g, "-"), dateValue);
         if (isFutureDate) {
             const postObject = {
                 bookingType: "hotel",
@@ -48,14 +51,56 @@ const SearchHolidays = () => {
             const response  = await fetch('https://www.virginholidays.co.uk/cjs-search-api/search', requestOptions);
             const data = await response.json();
             console.log("response", data);
-            if (data && data !== "") {
+            if (data && data.length !== 0) {
                 setReturnData(data);
-                setIsLoading(!isLoading);
-                console.log("isLoading", isLoading);
             }
 
             }
     }
+    
+    function filterResults(event) {
+        setIsFiltered(true);
+        let hotels = [];
+        let content = [];
+        let facilities = [];
+        for (let i = 0; i < returnData.holidays.length; i++) {
+            hotels.push(returnData.holidays[i].hotel);
+            
+        }
+        for (let i = 0; i < hotels.length; i++) {
+            content.push(hotels[i].content);
+            facilities.push(hotels[i].content.hotelFacilities);
+        }
+        console.log("hotels", hotels);
+        console.log("content", content);
+        console.log("facilities", facilities);
+        if (event.target.value === "price") {
+            console.log("isFilteredByPrice", isFilteredByPrice)
+            setIsFilteredByPrice(true);
+            setIsFilteredByStarRating(false);
+            setIsFilteredByFacilities(false);
+            console.log("paddy", event.target.value, isFilteredByPrice);
+            sortedArray = returnData.holidays;
+            sortedArray?.sort((a, b) => (a.pricePerPerson > b.pricePerPerson ? -1 : 1))
+
+        } if (event.target.value === "star-rating") {
+            setIsFilteredByPrice(false);
+            setIsFilteredByStarRating(true);
+            setIsFilteredByFacilities(false);
+            sortedArray = content;
+            sortedArray?.sort((a, b) => (a.starRating > b.starRating ? -1 : 1))
+
+        } if (event.target.value === "facilities") {
+            setIsFilteredByPrice(false);
+            setIsFilteredByStarRating(false);
+            setIsFilteredByFacilities(true);
+            sortedArray = content;
+            sortedArray.hotelFacilities?.sort((a, b) => (a.length > b.length ? -1 : 1))
+        }
+
+            setSortedArray(sortedArray);
+
+    };
 	return (
         <div class="container">
             <div class="row">
@@ -85,13 +130,59 @@ const SearchHolidays = () => {
                         </div>
                         </div>
                 </form>
+                {returnData &&
+                    <>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                            <button class="btn btn-primary me-md-2" value="price" onClick={filterResults} type="button">Filter By Price PP</button>
+                            <button class="btn btn-success me-md-2" value="facilities" onClick={filterResults} type="button">Filter By Hotel Facilities</button>
+                            <button class="btn btn-info me-md-2" value="star-rating" type="button" onClick={filterResults}>Filter By Star Rating</button>
+
+                        </div></>}
             </div>
-            {returnData && <div class="row">
-                <div class="col-8 justify-content-center p-2">
-                    {returnData.holidays.map((currentHol) => (
+            {returnData && !isFiltered && <div class="row">
+                <div class="col-8 justify-content-center">
+                    {returnData.holidays.map((currentHol, index) => (
                         <>
-                            <h2 class="title">{currentHol.hotel.name}</h2>
-                            <p class="text">{currentHol.hotel.content.atAGlance}</p>
+                            {currentHol.hotel && currentHol.hotel.name &&<h2 class="title">Name: {currentHol.hotel.name}</h2>}
+                            {currentHol.totalPrice && <h4 class="total-price">Total Price: {currentHol.totalPrice}</h4>}
+                            {currentHol.hotel && currentHol.hotel.content 
+                            && currentHol.hotel.content.hotelDescription && <p>Description: {currentHol.hotel.content.hotelDescription}</p>}
+                            <hr style="border-top: dotted 1px;" />
+                        </>))}
+                </div>
+            </div>}
+            {console.log("sortedArray",sortedArray )}
+            {sortedArray && sortedArray.length !==0 && sortedArray.forEach(element => {
+                console.log("element", element.hotel);
+            })}
+
+            {sortedArray && sortedArray.length !== 0 && isFilteredByPrice && <div class="row">
+                <div class="col-8 justify-content-center p-2">
+                    {sortedArray.map((currentEl, index) => (
+                        <>
+                            {currentEl.hote && currentEl.hote.name && <h4>Name: {currentEl.hotel.name}</h4>}
+                            {currentEl.pricePerPerson && <h4 class="total-price">Price Per Person: {currentEl.pricePerPerson}</h4>}
+                            <hr style="border-top: dotted 1px;" />
+                        </>))}
+                </div>
+            </div>}
+            {sortedArray && sortedArray.length !==0 && isFilteredByStarRating && <div class="row">
+                <div class="col-8 justify-content-center p-2">
+                    {sortedArray.map((currentHol, index) => (
+                        <>
+                            {currentHol.name && <h2 class="title">Name: {currentHol.name}</h2>}
+                            { currentHol.starRating && <h4 class="total-price">Rating: {currentHol.starRating}</h4>}
+                            <hr style="border-top: dotted 1px;" />
+                        </>))}
+                </div>
+            </div>}
+            {sortedArray && sortedArray.length !==0 && isFilteredByFacilities && <div class="row">
+                <div class="col-8 justify-content-center p-2">
+                    {sortedArray.map((currentHol, index) => (
+                        <>
+                            {currentHol.name && <h2 class="title">Name: {currentHol.name}</h2>}
+                            {currentHol.hotelFacilities && <h4 class="total-price">Facilities: {currentHol.hotelFacilities}</h4>}
+                            <hr style="border-top: dotted 1px;" />
                         </>))}
                 </div>
             </div>}
